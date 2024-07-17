a4d3ff392f6abd698e210ef2bab08e65f33800c2
jesongit <188380780@qq.com>
2024-07-17 10:38:27 +0800

    update dockerfile

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,5 @@
 wget -O nav.zip https://github.com/jesongit/nav/archive/refs/heads/master.zip
-unzip -o nav.zip -d .
+unzip -o nav.zip
+cd nav-master
 pip install -r requirements.txt
 python main.py
